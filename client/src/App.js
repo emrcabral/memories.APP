7311c15432adaae4c2e7ca1223c18e698963744c
Erik Cabral <erikcabralcs@gmail.com>
2024-04-28 20:34:31 -0700

    20240428.2 started AUTH. unknown error

--- client/src/App.js
+++ client/src/App.js
@@ -5,9 +5,9 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { ThemeProvider } from '@mui/material/styles';
 import { useTheme } from '@mui/material/styles';
 
-
 import Navbar from "./components/Navbar/Navbar";
 import Home from "./components/Home/Home";
+import Auth from "./components/Auth/Auth";
 
 
 const App = () => {
@@ -22,7 +22,6 @@ const App = () => {
                         <Route path="/" exact component={Home} />
                         <Route path="/auth" exact component={Auth} />
                     </Routes>
-                    <Home />
                 </Container>
             </BrowserRouter>
         </ThemeProvider>
